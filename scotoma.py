019a14989c6115548a433db22293f19ed2f029f1
Dorota Kopczyk <dorotakopczyk@Dorotas-MacBook-Air.local>
2024-04-03 12:04:25 -0500

    draw scotoma

--- scotoma.py
+++ scotoma.py
@@ -1,15 +1,19 @@
 import flet as ft
 import cv2
+import numpy as np
 import base64
 
 
 # initialize camera
 cap = cv2.VideoCapture(0)
+# Control the size of the scotoma
+scotoma_radius = 180
 
 class ScotomaWrapper(ft.UserControl):
 
     def __init__(self):
         super().__init__()
+        self.tangent_color = (0, 0, 255)  # red
 
 
     def did_mount(self):
@@ -17,25 +21,23 @@ class ScotomaWrapper(ft.UserControl):
 
     # This will the the entry point to rendering the camera image on the screen.
     def update_timer(self):
-        image_path = "pit.png" 
-        image = cv2.imread(image_path) # cv2.imread("pit.png", cv2.IMREAD_UNCHANGED)
-        image = cv2.resize(image, (0, 0), fx=0.25, fy=0.25)
-        if image is None:
-            print("Error: Could not load image")
-            return
-        print(f"Height:\t\t {image.shape[0]}") 
-        print(f"Width:\t\t {image.shape[1]}") 
-        print(f"Channel:\t {image.shape[2]}")
-
         while True:
-            _,frame=cap.read()
+            _, frame = cap.read()
             image_dims = frame.shape
             image_height, image_width = image_dims[0], image_dims[1]
-            h, w, _ = image.shape 
-            frame[(image_height//2)-(h//2):(image_height//2)+(h//2), (image_width//2)-((w//2)+1):(image_width//2)+(w//2)] = image  # Adjust position as needed
-            
 
-            _,im_arr = cv2.imencode(".png", frame)
+            # DRAW SCOTOMA
+            circle_center = (image_width // 2, image_height // 2)
+            radius = scotoma_radius
+            cv2.circle(frame, circle_center, radius, (0, 0, 0), -1)  # -1 means filled circle
+
+            # DRAW ADJACANT TO SCOTOMA
+            tangent_radius = scotoma_radius / 4  # Adjust this as you want
+            tangent_center = (int(circle_center[0] + scotoma_radius + tangent_radius), circle_center[1])
+            cv2.circle(frame, tangent_center, int(tangent_radius), self.tangent_color, -1)
+
+
+            _, im_arr = cv2.imencode(".png", frame)
             im_b64 = base64.b64encode(im_arr)
             self.img.src_base64 = im_b64.decode('utf-8')
             self.update()
@@ -46,7 +48,7 @@ class ScotomaWrapper(ft.UserControl):
             self.img
         ])
     
-section = ft.Container(
+camera_container = ft.Container(
     margin = ft.margin.only(bottom=40),
     content=ft.Row([
         ft.Card(
@@ -56,7 +58,9 @@ section = ft.Container(
                 padding=10,
                 border_radius=ft.border_radius.all(20),
                 content=ft.Column([
-                    ft.Text("Camera", size=30, weight="bold",
+                    ft.Text("Camera", 
+                            size=30, 
+                            weight="bold",
                             color="white"
                             ),
                         ScotomaWrapper()
@@ -65,13 +69,45 @@ section = ft.Container(
         )
     ], alignment='center')
 )
+
+
+
 def main(page: ft.Page):
+    page.title = "Visualize Scotoma"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.padding=50
     page.window_left=page.window_left+100
     page.theme_mode='light'
-    page.add(section)
-if __name__ == "__main__":
 
-    ft.app(target=main)
+    # Allow the user to change the size of the scotoma
+    txt_number = ft.TextField(value="180", text_align=ft.TextAlign.RIGHT, width=100)
+    def decrement_click(e):
+        global scotoma_radius
+        txt_number.value = str(int(txt_number.value) - 1)
+        scotoma_radius = int(txt_number.value)
+        page.update()
+
+    def increment_click(e):
+        global scotoma_radius
+        txt_number.value = str(int(txt_number.value) + 1)
+        scotoma_radius = int(txt_number.value)
+        page.update()
+    
+    # Increment Text Field
+    page.add(
+        ft.Row(
+            [
+                ft.IconButton(ft.icons.REMOVE, on_click=decrement_click),
+                txt_number,
+                ft.IconButton(ft.icons.ADD, on_click=increment_click),
+            ]
+        )
+    )
+    # Main camera frame
+    page.add(camera_container)
+    
+if __name__ == "__main__":
+    ft.app(target=main) # <--- Visualize in native OS window
+    # ft.app(target=main, view=ft.AppView.WEB_BROWSER) # <--- Visualize in a web browser
     cap.release()
     cv2.destroyAllWindows()
