e6ffbe67caca0ffe16aaed8fe8586d0cbc7a90aa
Dorota Kopczyk <dorotakopczyk@infra04-wg012.eduroam.wireless.umn.edu>
2024-04-09 11:41:23 -0500

    try out tesseract

--- scotoma.py
+++ scotoma.py
@@ -2,12 +2,18 @@ import flet as ft
 import cv2
 import numpy as np
 import base64
+import pytesseract
 
 
+# Finding text: https://medium.com/@draj0718/text-recognition-and-extraction-in-images-93d71a337fc8
 # initialize camera
 cap = cv2.VideoCapture(0)
 # Control the size of the scotoma
-scotoma_radius = 180
+scotoma_radius = 100
+
+# Tesseract path
+# I DONT KNOW HOW TO MAKE THIS LESS SLOPPY
+pytesseract.pytesseract.tesseract_cmd = '/opt/homebrew/bin/tesseract'
 
 class ScotomaWrapper(ft.UserControl):
 
@@ -26,6 +32,32 @@ class ScotomaWrapper(ft.UserControl):
             image_dims = frame.shape
             image_height, image_width = image_dims[0], image_dims[1]
 
+            # OCR STUFF 
+            # pytessercat, look for some text, but do some preprocessing first to improve accuracy
+            # oem 3 is the default of what's available
+            #     1 is (fanciest) Neural nets LSTM engine mode (Tesseract 4 new LSTM engine)
+            # psm 7 treat the image as a single text line
+            #     3 automated, no assumptions made
+            config = '-l eng --oem 1 --psm 3'
+            # get grayscale image
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # noise removal
+            noise=cv2.medianBlur(gray, 3) # Kernel size needs to be an odd number
+            # gaussian blurring MAKES THINGS WORSE
+            # blurred_image = cv2.GaussianBlur(noise, (5, 5), 0)
+            # Up the contrast maybe to improve accuracy?
+            # equalized_image = cv2.equalizeHist(gray)
+            # Different thresholding types to experiment with
+            # ADAPTIVE_THRESH_MEAN_C
+            # THRESH_OTSU
+            # ADAPTIVE_THRESH_GAUSSIAN_C
+            # thresholding to handle colored images
+            # _, thresholded_img = cv2.threshold(noise, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+            _, thresholded_img = cv2.threshold(noise, 0, 255, cv2.THRESH_OTSU)
+            text = pytesseract.image_to_string(thresholded_img, config=config)
+            if text:
+                print(text)
+
             # DRAW SCOTOMA
             circle_center = (image_width // 2, image_height // 2)
             radius = scotoma_radius
@@ -35,7 +67,6 @@ class ScotomaWrapper(ft.UserControl):
             tangent_radius = scotoma_radius / 4  # Adjust this as you want
             tangent_center = (int(circle_center[0] + scotoma_radius + tangent_radius), circle_center[1])
             cv2.circle(frame, tangent_center, int(tangent_radius), self.tangent_color, -1)
-
 
             _, im_arr = cv2.imencode(".png", frame)
             im_b64 = base64.b64encode(im_arr)
